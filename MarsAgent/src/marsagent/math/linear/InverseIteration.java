417bfcfc75c86e1374f8f3078c336f8766e422e8
sehlstrom <alexander@sehlstrom.se>
2013-04-15 17:05:42 +0200

    Errors and documentation * Correction of error when using integers in Math.pow: Math.pow(..., 1/2) changed to Math.pow(..., 0.5). * Addition of possibility to solve non-generalized problems with a shift * General comments added.

--- MarsAgent/src/marsagent/math/linear/InverseIteration.java
+++ MarsAgent/src/marsagent/math/linear/InverseIteration.java
@@ -3,6 +3,8 @@
  */
 package marsagent.math.linear;
 
+import java.util.ArrayList;
+
 import marsagent.math.exception.DimensionMissmatchException;
 import marsagent.math.exception.NonSquareMatrixException;
 
@@ -17,7 +19,6 @@ import org.jblas.Solve;
  * using the inverse iteration method with Rayleigh quotient.
  * 
  * <p>The <code>InverseIteration</code> solves the following generalized eigenvalue problem:</p>
- * 
  * <math xmlns="http://www.w3.org/1998/Math/MathML" display="block">
  *   <mo>(</mo>
  *     <mn><b>A</b></mn>
@@ -28,16 +29,27 @@ import org.jblas.Solve;
  *   <mn><b>0</b></mn>
  * </math>
  * 
+ * <p>where it is assumed that the sought eigenvalue <math xmlns="http://www.w3.org/1998/Math/MathML" display="inline">
+ * <mi>&lambda;<sub><mn>1</mn></sub></mi></math> is distinct and that the eigenvalues are ordered according to</p>
+ * 
+ * <math xmlns="http://www.w3.org/1998/Math/MathML" display="block">
+ *   <mi>&lambda;<sub><mn>1</mn></sub></mi><mo>&lt;</mo>
+ *   <mi>&lambda;<sub><mn>2</mn></sub></mi><mo>&leq;</mo>
+ *   <mi>&lambda;<sub><mn>3</mn></sub></mi><mo>&leq;</mo>
+ *   <mo>...</mo><mo>&leq;</mo>
+ *   <mi>&lambda;<sub><mi>N</mi></sub></mi>
+ * </math>
+ * 
  * <p>The eigenvalue <math xmlns="http://www.w3.org/1998/Math/MathML" display="inline"><mi>&lambda;<sub><mn>1</mn></sub><sup><mn>*</mn></sup></mi></math>
  * is approximated such that
- * <math xmlns="http://www.w3.org/1998/Math/MathML" display="inline"><mi>&lambda;<sub><mn>1</mn></sub><sup><mn>*</mn></sup></mi><mo>&gt;</mo><mi>&lambda;<sub><mn>1</mn></sub></mn></math>
+ * <math xmlns="http://www.w3.org/1998/Math/MathML" display="inline"><mi>&lambda;<sub><mn>1</mn></sub></mn><mo>&leq;</mo><mi>&lambda;<sub><mn>1</mn></sub><sup><mn>*</mn></sup></mi></math>
  * where <math xmlns="http://www.w3.org/1998/Math/MathML" display="inline"><mi>&lambda;<sub><mn>1</mn></sub></mn></math> is the true lowest eigenvalue.</p>
  * 
  * <p>It is possible to solve for other approximations of the eigenvalue other
  * than the lowest eigenvalue by shifting the eigenvalue spectrum using
  * <math xmlns="http://www.w3.org/1998/Math/MathML" display="inline"><mi>&mu;</mi><mo>&gt;</mo><mi>&lambda;<sub><mi>n</mi></sub></mi><mo>&gt;</mo><mi>&lambda;<sub><mn>1</mn></sub></mi></math>
  * where <math xmlns="http://www.w3.org/1998/Math/MathML" display="inline"><mi>&lambda;<sub><mi>n</mi></sub></mn></math> is the sought eigenvalue.
- * The following problem is then solved:
+ * The following problem is then solved:</p>
  * 
  * <math xmlns="http://www.w3.org/1998/Math/MathML" display="block">
  *   <mo>[</mo>
@@ -76,27 +88,38 @@ public class InverseIteration {
 	private static final int MAX_ITER = 10;
 
 	/** Default change tolerance */
-	private static final double TOL = 1e-3;
+	private static final double TOL = 1e-9;
 
 	public static void main(String[] args) {
-		int m = 100;
 		InverseIteration ii = new InverseIteration();
 
-		DoubleMatrix a = DoubleMatrix.eye(m).mul(Math.random());
-		DoubleMatrix b = DoubleMatrix.eye(m).mul(Math.random());
+		DoubleMatrix a = new DoubleMatrix(new double[][]{
+				{ 2, -1,  0,  0},
+				{-1,  2, -1,  0},
+				{ 0, -1,  2, -1},
+				{ 0,  0, -1,  2}});
 
+		DoubleMatrix b = DoubleMatrix.eye(a.rows);
+
+		System.out.println("GENERALIZED SYSTEM");
 		ResultStructure rs1 = ii.compute(a, b);
 
 		System.out.println("Inverse iteration results");
 		System.out.println("lambda = " + rs1.lambda);
 		System.out.println("phi(1) = " + rs1.phi.get(0));
+		System.out.println("phi(2) = " + rs1.phi.get(1));
 
 		DoubleMatrix rs2lambda = Eigen.symmetricGeneralizedEigenvalues(a, b);
 		DoubleMatrix[] rs2phi = Eigen.symmetricGeneralizedEigenvectors(a, b);
 
 		System.out.println("\njBlas solver results");
-		System.out.println("lambda = " + rs2lambda.min());
+		DoubleMatrix rs2lambdapositive = rs2lambda.ge(0).dup();
+		rs2lambda = rs2lambda.get(rs2lambdapositive);
+		System.out.println("lambda = " + rs2lambda.min() + " (the " + rs2lambda.argmin() + "th eigenvalue)");
 		System.out.println("phi(1) = " + rs2phi[rs2lambda.argmin()].get(0));
+		System.out.println("phi(2) = " + rs2phi[rs2lambda.argmin()].get(1));
+
+		System.out.println("\nRatio: " + (rs1.lambda/rs2lambda.min()));
 	}
 
 	/** Maximum number of iterations */
@@ -121,6 +144,45 @@ public class InverseIteration {
 	public InverseIteration(double tol, int maxiter) {
 		this.tol = tol;
 		this.maxiter = maxiter;
+	}
+
+	/**
+	 * Solves the lowest eigenvalue and it's corresponding eigenvector for the
+	 * eigenvalue problem.
+	 * @param a		square matrix <code>(m x m)</code>
+	 * @return result
+	 * @throws NonSquareMatrixException if <code>a</code> or <code>b</code> is not square
+	 * @throws DimensionMissmatchException if any input has missmatching dimensions
+	 */
+	public ResultStructure compute(DoubleMatrix a) {
+		return compute(a, DoubleMatrix.eye(a.rows), 0, DoubleMatrix.ones(a.rows,1));
+	}
+
+	/**
+	 * Solves the lowest eigenvalue and it's corresponding eigenvector for the
+	 * eigenvalue problem with spectrum shift.
+	 * @param a		square matrix <code>(m x m)</code>
+	 * @param mu	spectrum shift
+	 * @return result
+	 * @throws NonSquareMatrixException if <code>a</code> or <code>b</code> is not square
+	 * @throws DimensionMissmatchException if any input has missmatching dimensions
+	 */
+	public ResultStructure compute(DoubleMatrix a, double mu) {
+		return compute(a, DoubleMatrix.eye(a.rows), mu, DoubleMatrix.ones(a.rows,1));
+	}
+
+	/**
+	 * Solves the lowest eigenvalue and it's corresponding eigenvector for the
+	 * eigenvalue problem with spectrum shift and initial guess mode.
+	 * @param a		square matrix <code>(m x m)</code>
+	 * @param mu	spectrum shift
+	 * @param phi0	initial guess mode <code>(m x 1)</code>
+	 * @return result
+	 * @throws NonSquareMatrixException if <code>a</code> or <code>b</code> is not square
+	 * @throws DimensionMissmatchException if any input has missmatching dimensions
+	 */
+	public ResultStructure compute(DoubleMatrix a, double mu, DoubleMatrix phi0) {
+		return compute(a, DoubleMatrix.eye(a.rows), mu, phi0);
 	}
 
 	/**
@@ -164,6 +226,7 @@ public class InverseIteration {
 	 * @throws DimensionMissmatchException if any input has missmatching dimensions
 	 */
 	public ResultStructure compute(DoubleMatrix a, DoubleMatrix b, double mu, DoubleMatrix phi0) {
+		// Check input
 		if (!a.isSquare()) {
 			throw new NonSquareMatrixException("The a-matrix has to be square");
 		}
@@ -180,44 +243,53 @@ public class InverseIteration {
 			throw new DimensionMissmatchException("The phi0-vector has to have the same number of rows as the a-matrix");
 		}
 
-		ResultStructure result = new ResultStructure();
-		DoubleMatrix phi = phi0.dup();
-		DoubleMatrix phi_s;
-		DoubleMatrix phi_hat;
+		// Setup iteration arrays with initial guesses.
+		ArrayList<DoubleMatrix> phi = new ArrayList<DoubleMatrix>(maxiter);
+		phi.add(0, phi0.dup());
+
+		ArrayList<Double> lambda = new ArrayList<Double>(maxiter);
+		lambda.add(0, -1e3);
+
+		// Shift the a-matix
+		// MATLAB: a_hat = a - mu * b;
 		DoubleMatrix a_hat = a.sub(b.mul(mu));
 
+		// Give name to variables and initiate some of them
+		DoubleMatrix phi_hat, phi_s;
+		ResultStructure result = new ResultStructure();
 		boolean run = true;
 		int s = 0;
-		double lambda = 0;
-		double lambda_s = 0;
 
 		// Iterate
 		while (run) {
 			// Guess mode of step s
-			phi_s = phi;
+			// MATLAB: phi_s = phi(:,s);
+			phi_s = phi.get(s);
 
 			// Shifted guess mode of step s
-			phi_hat = Solve.solveSymmetric(a_hat, b.mmul(phi_s));
+			// MATLAB: phi_hat = a_hat \ (b * phi_s);
+			phi_hat = Solve.solve(a_hat, b.mmul(phi_s));
 
-			// Eigenvalue of step s
-			lambda_s = phi_hat.transpose().mmul(b).mmul(phi_s).div(phi_hat.transpose().mmul(b).mmul(phi_hat)).get(0);
+			// Eigenvalue of step s+1
+			// MATLAB: lambda(s+1) = (phi_hat' * b * phi_s) / (phi_hat' * b * phi_hat);
+			lambda.add(s+1, phi_hat.transpose().mmul(b).mmul(phi_s).div(phi_hat.transpose().mmul(b).mmul(phi_hat)).get(0));
 
-			// Eigenvector of step s
-			phi = phi_hat.div(Math.pow(phi_hat.transpose().mmul(b).mmul(phi_hat).get(0), 1/2));
+			// Eigenvector of step s+1
+			// MATLAB: phi(:,s+1) = phi_hat / (phi_hat' * b * phi_hat)^(1/2);
+			phi.add(s+1, phi_hat.div(Math.sqrt(phi_hat.transpose().mmul(b).mmul(phi_hat).get(0))));
 
 			// Check stop criterion
-			run = ((Math.abs(lambda_s-lambda) < tol) || (s > maxiter) ? false : true );
-
-			// Save iteration lambda value
-			lambda = lambda_s;
+			run = ((Math.abs(lambda.get(s+1)-lambda.get(s)) < tol) || (s > maxiter) ? false : true );
 
 			// Count
 			s ++;
 		}
 
-		result.lambda = lambda_s + mu;
-		result.phi = phi;
+		// Shift the spectrum back again and save results.
+		result.lambda = lambda.get(s) + mu;
+		result.phi = phi.get(s);
 
+		// Return results
 		return result;
 	}
 
